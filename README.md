51f6e4a82a891587b8db237904b5290ca5fb960d
michalskrzynski <michal.skrzynski@gmail.com>
2022-12-21 14:34:06 +0100

    mvp ready

---
+++ README.md
@@ -0,0 +1,5 @@
+This project has been developed with Vite.
+
+Therefore to run it, run the Vite provided server and then:
+
+http://127.0.0.1:5173/index.html
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,40 +1,65 @@
 import React from "react";
-import memeData from '../meme-data';
-
+import memeData from "../meme-data";
 
 export default function Main() {
-
   let [memes, setMemes] = React.useState(memeData.data.memes);
 
   const getMemeImageUrl = () => {
-    return memes[ Math.floor( Math.random() * memes.length ) ].url;
-  }
+    return memes[Math.floor(Math.random() * memes.length)].url;
+  };
 
-  let [meme, setMeme] = React.useState( { 
+  let [meme, setMeme] = React.useState({
     url: getMemeImageUrl(),
-    topText: '',
-    bottomText: '' 
-  } );
+    topText: "",
+    bottomText: "",
+  });
 
-  
-  function newImageButtonClicked() {
-    setMeme( previousMeme => {
-      return {
-        ...previousMeme, 
-        url: getMemeImageUrl()
-      }
-    } );
+  function handleChange(e) {
+    let { name, value } = e.target;
+    setMeme( prevMeme => ({ ...prevMeme, [name]: value }) );
   }
 
+  function newImageButtonClicked() {
+    setMeme((previousMeme) => {
+      return {
+        ...previousMeme,
+        url: getMemeImageUrl(),
+      };
+    });
+  }
 
   return (
     <main>
       <div className="content--grid">
-        <div><input type="text" name="line1" id="memeLine1" placeholder='Shut up' /></div>
-        <div><input type="text" name="line2" id="memeLine2" placeholder='and take my money'/></div>
-        <div className="content--row2"><button id="newImageButton" onClick={newImageButtonClicked}>Get a new image</button></div>
+        <div>
+          <input
+            type="text"
+            name="topText"
+            value={meme.topText}
+            onChange={handleChange}
+            placeholder="Shut up"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
+            placeholder="and take my money"
+          />
+        </div>
+        <div className="content--row2">
+          <button id="newImageButton" onClick={newImageButtonClicked}>
+            Get a new image
+          </button>
+        </div>
       </div>
-      <img className="content--image" src={meme.url} />
+      <div className="content--image">
+        <img src={meme.url} />
+        <span className="content--text content--line1">{meme.topText}</span>
+        <span className="content--text content--line2">{meme.bottomText}</span>
+      </div>
     </main>
-  )
+  );
 }
